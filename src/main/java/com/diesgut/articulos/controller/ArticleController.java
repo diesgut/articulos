7cb6ea4d743ec3d0a10b3df423189d372768dbb6
Usuario <renato_2608@hotmail.com>
2019-09-21 17:58:46 -0500

    completando el crud, falta validaciones al guardar y verificar el update

--- src/main/java/com/diesgut/articulos/controller/ArticleController.java
+++ src/main/java/com/diesgut/articulos/controller/ArticleController.java
@@ -3,11 +3,20 @@ package com.diesgut.articulos.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.diesgut.articulos.model.Article;
 import com.diesgut.articulos.service.impl.ArticleServiceImp;
@@ -34,6 +43,45 @@ public class ArticleController {
 		ModelAndView model = new ModelAndView(INDEX_VIEW);
 		model.addObject("articles", articles);
 		return model;
+	}
+
+	@GetMapping("/articles")
+	public ModelAndView getAllArticles() {
+		List<Article> articles = articleService.getAllArticles();
+		ModelAndView model = new ModelAndView(ARTICLE_PAGE_VIEW);
+		model.addObject("articles", articles);
+		return model;
+	}
+
+	@GetMapping("/article/new")
+	public String newArticle(Model model) {
+		model.addAttribute("article", new Article());
+		return ARTICLE_ADD_FORM_VIEW;
+	}
+
+	@PostMapping("/article/create")
+	public String createArticle(@Valid Article article, BindingResult result, Model model, RedirectAttributes attr) {
+		model.addAttribute("article", new Article());
+		if (result.hasErrors()) {
+			attr.addFlashAttribute("org.springframework.valdation.BindingResult", result);
+			attr.addFlashAttribute("article", article);
+			return "redirect:/article/new";
+		}
+		Article newArticle = articleService.createArticle(article);
+		model.addAttribute("article", newArticle);
+		return "redirect:/article/" + newArticle.getArticleId();
+	}
+
+	@GetMapping("/article/{id}")
+	public String getArticleById(@PathVariable(value = "id") Long articleId, Model model) {
+		model.addAttribute("article", articleService.findById(articleId));
+		return ARTICLE_VIEW;
+	}
+
+	@GetMapping("/article/{id}/edit")
+	public String editArticle(@PathVariable(value = "id") Long articleId, Model model) {
+		model.addAttribute("article", articleService.findById(articleId));
+		return ARTICLE_ADD_FORM_VIEW;
 	}
 
 }
